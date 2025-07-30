f7b7adc9387cb89008cc64f2bc26737770c057da
Rıdvan Aslan <ridvan_aslan.tr@icloud.com>
2025-07-30 18:33:43 +0300

    introduce Python reference

---
+++ reference.py
@@ -0,0 +1,21 @@
+#value types => numbers, strings
+
+x = 5
+y = 25
+
+x = y
+
+y = 10
+
+# print(x, y)
+
+#reference types => lists
+
+a = ["apple", "banana", "cherry"]
+b = ["apple", "banana", "cherry"]
+
+a = b
+
+b[0] = "orange"
+
+print(a, b)
