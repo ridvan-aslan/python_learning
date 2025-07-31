6623352258bbfb17561ec44244fbc01056e8bf20
Rıdvan Aslan <ridvan_aslan.tr@icloud.com>
2025-07-31 12:35:46 +0300

    introduce Python comparisons

---
+++ comparison.py
@@ -0,0 +1,23 @@
+# username, password => database 
+
+# "ridvanaslan", "12345"
+
+a, b, c, d = 5, 5, 10, 4
+
+username = "ridvanaslan"
+password = "12345"
+
+result = (a == b)
+result = (a == c)
+result = "ridvanaslan" == username
+result = "rdvnasln" == username
+result = (a != b)
+result = (a != c)
+result = (a > c)
+result = (a <= b)
+result = (True == 1)
+result = (False == 0)
+result = False + True + 40
+
+
+print(result)
