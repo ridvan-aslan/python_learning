3d25f800bd1a78da91230615002cbae97b346c17
Rıdvan Aslan <ridvan_aslan.tr@icloud.com>
2025-07-31 19:39:12 +0300

    introduce Python logical operators

---
+++ logical.py
@@ -0,0 +1,13 @@
+x = 6
+
+result = 5 < x < 10
+
+result = (x > 5) and (x < 10)
+
+result = (x > 0) or (x % 2 == 0)
+
+result = not(x > 0)
+
+result = (x > 5) or (x < 10) and (x % 2 == 0)
+
+print(result)
