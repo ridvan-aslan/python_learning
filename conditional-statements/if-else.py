63a02faa5afbbbdd0a27ec5541e62b847c6bba8f
Rıdvan Aslan <ridvan_aslan.tr@icloud.com>
2025-08-03 19:00:04 +0300

    introduce Python if-else conditionals

---
+++ conditional-statements/if-else.py
@@ -0,0 +1,10 @@
+username = "ridvanaslan"
+password = "1234"
+
+if username == "ridvanaslann":
+    if password == "12345":
+        print("Welcome!")
+    else:
+        print("Your password is incorrect.")
+else:
+    print("Your information is incorrect.")
