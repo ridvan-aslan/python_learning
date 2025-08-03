9139a23fc3fcc079625d0d54b253ef3ccbf62536
Rıdvan Aslan <ridvan_aslan.tr@icloud.com>
2025-08-03 19:08:43 +0300

    introduce Python if-elif conditionals

---
+++ conditional-statements/if-elif.py
@@ -0,0 +1,18 @@
+# x = int(input("Enter first number: "))
+# y = int(input("Enter second number: "))
+
+# if x > y :
+#     print("x is bigger than y")
+# elif x == y :
+#     print("x is the same as y")
+# else:
+#     print("x is smaller than y")
+
+number = int(input("Enter a number: "))
+
+if number > 0 :
+    print(f"{number} is positive")
+elif number == 0 :
+    print(f"{number} is zero")
+else:
+    print(f"{number} is negative")
