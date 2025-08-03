f66077899f9649ed7d7e63f689e5c233e802eaac
Rıdvan Aslan <ridvan_aslan.tr@icloud.com>
2025-08-03 16:25:08 +0300

    introduce other logical operators in Python

---
+++ others.py
@@ -0,0 +1,19 @@
+# Identity Operator : is
+
+x = y = [1, 2, 3]
+z = [1, 2, 3]
+
+# print(x == y)
+# print(x == z)
+# print(x is y)
+# print(x is z)
+
+# Membership Operator: in
+
+x = ["Apple", "Banana"]
+
+print("Apple" in x)
+
+name = "Rıdvan"
+
+print("R" in name)
