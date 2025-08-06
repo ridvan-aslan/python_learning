c892f0e6ae26a9c88b542c2b7a0089197f901a53
Rıdvan Aslan <ridvan_aslan.tr@icloud.com>
2025-08-06 15:32:55 +0300

    introduce Python while loops

---
+++ loops/while.py
@@ -0,0 +1,17 @@
+# x = 1
+
+# while x <= 100:
+#     if x % 2 == 1:
+#         print(f"The number is odd {x}")
+#     else:
+#         print(f"The number is even {x}")
+#     x += 1
+
+# print("finished")
+
+name = ""
+
+while not name :
+    name = input("Enter your name: ").strip()
+
+print(f"Welcome {name}")
