9423d6322fd16d4492b024ce698522652778adb4
Rıdvan Aslan <ridvan_aslan.tr@icloud.com>
2025-08-06 14:25:42 +0300

    introduce Python for loops

---
+++ loops/for.py
@@ -0,0 +1,24 @@
+# numbers = [1, 2, 3, 4, 5]
+
+# for x in numbers:
+#     print("Hello")
+
+# names = ["Rıdvan", "Çınar", "Sadık"]
+
+# for name in names:
+#     print(f"My name is {name}.")
+
+# name = "Rıdvan Aslan"
+
+# for n in name:
+#     print(n)
+
+# tuple = [(1,4), (4,2), (5,1), (3,4)]
+
+# for a, b in tuple:
+#     print(a, b)
+
+d = {"k1": 1, "k2": 2, "k3": 3 }
+
+for key, value in d.items():
+    print(value)
