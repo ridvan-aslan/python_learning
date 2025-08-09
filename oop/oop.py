9b7b32332126c5ab6df17c9a66c55223382a39a4
Rıdvan Aslan <ridvan_aslan.tr@icloud.com>
2025-08-09 19:02:56 +0300

    introduce Python oop

---
+++ oop/oop.py
@@ -0,0 +1,13 @@
+# Object Oriented Programming (OOP) in Python
+
+# class => Person(name, age)
+# instance(object)
+
+lst1 = [1, 2, 3]
+lst2 = [4, 5, 6]
+
+
+result = type(lst1)
+result = type(lst2)
+
+print(result)  # Output: <class 'list'>
