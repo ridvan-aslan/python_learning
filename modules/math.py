783496efea542e481e06da49d8527b8173878fb4
Rıdvan Aslan <ridvan_aslan.tr@icloud.com>
2025-08-14 22:40:45 +0300

    introduce Python modules

---
+++ modules/math.py
@@ -0,0 +1,27 @@
+# Approach 1
+
+# import math
+# import math as m
+
+# value = dir(math)
+# value = help(math)
+# value = help(math.sqrt)
+# value = math.sqrt(16)
+# value = math.factorial(5)
+# value = math.floor(5.9)
+# value = math.ceil(5.1)
+
+# value = m.pi
+
+# Approach 2
+
+def sqrt(x):
+    return "sqrt of " + str(x)
+
+# from math import *
+from math import sqrt, factorial, floor, ceil, pi
+
+value = sqrt(16)
+# value = factorial(5)
+
+print(value)
