a26c2cf100ebbe0061ff1b02dbe52bebf3748156
Rıdvan Aslan <ridvan_aslan.tr@icloud.com>
2025-08-16 09:18:13 +0300

    introduce Python file handling

---
+++ files/files.py
@@ -0,0 +1,12 @@
+# file = open('newfile.txt', 'w')
+# file = open(r"C:\Users\rdvnw\Desktop\newfile.txt", 'w')
+
+# file = open('files/newfile.txt', 'w', encoding='utf-8')
+# file.write('Rıdvan Aslan')
+
+# file = open('files/newfile.txt', 'a', encoding='utf-8')
+# file.write('Rıdvan Aslan\n')
+
+# file  = open('files/newfile2.txt', 'x', encoding='utf-8')
+
+# file.close()
