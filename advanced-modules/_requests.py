684c1fb8b2967c49705f4d4282573343bd1da1e9
Rıdvan Aslan <ridvan_aslan.tr@icloud.com>
2025-08-21 22:02:55 +0300

    introduce Requests module

---
+++ advanced-modules/_requests.py
@@ -0,0 +1,10 @@
+import requests
+import json
+
+result  = requests.get("https://jsonplaceholder.typicode.com/todos")
+result = json.loads(result.text)
+
+
+for i in result:
+    if i["userId"] == 3:
+        print(i)
