c377bc4864c9c0bdc088747c527d21cfdfb61d11
Rıdvan Aslan <ridvan_aslan.tr@icloud.com>
2025-09-18 12:30:07 +0300

    feat: add class for windows items

--- pyqt5/win-items.py
+++ pyqt5/win-items.py
@@ -2,39 +2,51 @@ import sys
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("PyQt5 App")
+        self.setGeometry(200, 200, 300, 300)
+        self.setToolTip("This is a QMainWindow")
+        self.init_UI()
+
+
+    def init_UI(self):
+        self.lbl_name = QtWidgets.QLabel(self)
+        self.lbl_name.setText("Name")
+        self.lbl_name.move(50, 30)
+
+        self.lbl_surname = QtWidgets.QLabel(self)
+        self.lbl_surname.setText("Surname")
+        self.lbl_surname.move(50, 70)
+
+        self.txt_name = QtWidgets.QLineEdit(self)
+        self.txt_name.move(120, 30)
+        
+        self.txt_surname = QtWidgets.QLineEdit(self)
+        self.txt_surname.move(120, 70)
+
+        self.lbl_result = QtWidgets.QLabel(self)
+        self.lbl_result.resize(200, 50)
+        self.lbl_result.move(120, 150)
+ 
+        self.btn_save = QtWidgets.QPushButton(self)
+        self.btn_save.setText("Save")
+        self.btn_save.move(120, 110)
+        self.btn_save.clicked.connect(self.btn_clicked)
+
+    def btn_clicked(self):
+        lbl_result = self.lbl_result
+        txt_name = self.txt_name.text()
+        txt_surname = self.txt_surname.text()
+        lbl_result.setText("Hello " + txt_name + " " + txt_surname)
+
+        
 def window():
     app = QApplication(sys.argv)
-    win = QMainWindow() 
-
-    win.setWindowTitle("PyQt5 App")
-    win.setGeometry(200, 200, 300, 300)
-    win.setToolTip("This is a QMainWindow")
-    
-    lbl_name = QtWidgets.QLabel(win)
-    lbl_name.setText("Name")
-    lbl_name.move(50, 30)
-
-    lbl_surname = QtWidgets.QLabel(win)
-    lbl_surname.setText("Surname")
-    lbl_surname.move(50, 70)
-
-    txt_name = QtWidgets.QLineEdit(win)
-    txt_name.move(120, 30)
-    
-    txt_surname = QtWidgets.QLineEdit(win)
-    txt_surname.move(120, 70)
-
-    def clicked():
-        print("Name: " + txt_name.text())
-        print("Surname: " + txt_surname.text())
-
-    btn_save = QtWidgets.QPushButton(win)
-    btn_save.setText("Save")
-    btn_save.move(120, 110)
-    btn_save.clicked.connect(clicked)
-
+    win = MyWindow() 
     win.show()
     sys.exit(app.exec_())
-
 
 window()
