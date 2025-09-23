5a9ffc3fce81742babf5804cec39cd56ff49e9fe
Rıdvan Aslan <ridvan_aslan.tr@icloud.com>
2025-09-24 00:08:06 +0300

    feat: add movie detail page

--- django/catalog/movies/views.py
+++ django/catalog/movies/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from .models import Movie
+from django.http import Http404
 
 
 # Create your views here.
@@ -12,7 +14,13 @@ def index(request):
     return render(request, 'movies/list.html', context)
 
 def detail(request, movie_id):
-    return render(request, 'movies/detail.html')
+
+    movie = get_object_or_404(Movie, pk=movie_id)
+    context = {
+        'movie': movie
+    }
+
+    return render(request, 'movies/detail.html', context)
 
 def search(request):
     return render(request, 'movies/search.html')
