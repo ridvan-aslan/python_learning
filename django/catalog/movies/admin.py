342a45d90f58f07d7ada7973f69006597b8d228c
Rıdvan Aslan <ridvan_aslan.tr@icloud.com>
2025-09-23 22:33:53 +0300

    feat: add features to admin panel

--- django/catalog/movies/admin.py
+++ django/catalog/movies/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 from .models import Movie
 
 
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'created_date', 'is_published')
+    list_display_links = ('id', 'title')
+    search_fields = ('title',)
+    list_filter = ('created_date',)
+    list_editable = ('is_published',)
+    list_per_page = 20
+
+
 # Register your models here.
-admin.site.register(Movie)
+admin.site.register(Movie, MovieAdmin)
--- django/catalog/movies/models.py
+++ django/catalog/movies/models.py
@@ -3,7 +3,13 @@ from django.db import models
 # Create your models here.
 
 class Movie(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    image = models.CharField(max_length=50)
-    created_date = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, verbose_name='Film Adı')
+    description = models.TextField(verbose_name='Film Açıklaması')
+    image = models.CharField(max_length=50, verbose_name='Film Resmi')
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name='Film Eklenme Tarihi')
+    is_published = models.BooleanField(default=True)
+
+
+
+    def __str__(self):
+        return f"{self.title}"
